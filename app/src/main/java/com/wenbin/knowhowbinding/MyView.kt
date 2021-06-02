c8f16629a32d601997e12a87bf8450f8796720c8
Andy177-Apps <leo55576@gmail.com>
2021-06-02 08:06:21 +0800

    Attempt to color gradient.

---
+++ app/src/main/java/com/wenbin/knowhowbinding/MyView.kt
@@ -0,0 +1,33 @@
+package com.wenbin.knowhowbinding
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.Nullable
+
+
+/**
+ * Created by fcn-mq on 2017/5/22.
+ * 自定义View
+ */
+class MyView : View {
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, @Nullable attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?, @Nullable attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //获取View的宽高
+        val width = width
+        val height = height
+        val colorStart = KnowHowBindingApplication.appContext.applicationContext.getColor(R.color.colorStart)
+        val color1 = KnowHowBindingApplication.appContext.applicationContext.getColor(R.color.color1)
+        val colorEnd = KnowHowBindingApplication.appContext.applicationContext.getColor(R.color.colorEnd)
+        val paint = Paint()
+        val backGradient: LinearGradient = LinearGradient(0F, 0F, 180F, 0F, intArrayOf(colorStart, color1, colorEnd), null, Shader.TileMode.CLAMP)
+        //        LinearGradient backGradient = new LinearGradient(0, 0, 0, height, new int[]{colorStart, color1 ,colorEnd}, null, Shader.TileMode.CLAMP);
+        paint.shader = backGradient
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+    }
+}
